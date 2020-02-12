6d4425abc917b356c4ee68989401bd97e03b5fca
Mehdi Mounsef <mounsif.mehdi@gmail.com>
2020-02-12 22:34:20 +0100

    Added AxeRotation and lateral offset when coming back

--- CharacterControl/AxeBehaviour.cs
+++ CharacterControl/AxeBehaviour.cs
@@ -20,8 +20,16 @@ public class AxeBehaviour : MonoBehaviour{
 
     [Header("Return Params")]
     public float ReturnSpeed; 
+    public float LateralReturnSpeed = 1f; 
+    public float CurveMultiplier = 1.5f; 
+    public AnimationCurve LateralInfluence;
+    float curve_eval;
+    float initial_sqr_dist; 
     Transform HandTarget;
     Vector3 to_hand_direction; 
+    Vector3 lateral_hand_direction; 
+
+    public float ReturnRotationSpeed = 5f; 
 
     [Header("Rotation Params")]
     public Vector3 RotationAxis = Vector3.right; 
@@ -42,6 +50,11 @@ public class AxeBehaviour : MonoBehaviour{
     public void ReturnOrder(Transform target){
         AxeState = AxeStates.Called; 
         HandTarget = target; 
+        to_hand_direction = HandTarget.position - transform.position; 
+        lateral_hand_direction = Quaternion.AngleAxis(90f, Vector3.up) * to_hand_direction; 
+        
+        curve_eval = 0f; 
+        initial_sqr_dist = Vector3.SqrMagnitude(to_hand_direction); 
     }
 
     public void AcknowledgeReturn(){
@@ -50,6 +63,7 @@ public class AxeBehaviour : MonoBehaviour{
 
     void Start(){
         AxeState = AxeStates.Held;  
+        RotationAxis = RotationAxis.normalized; 
         // kinematic_controller = GetComponent<CharacterController>(); 
     }
 
@@ -62,7 +76,12 @@ public class AxeBehaviour : MonoBehaviour{
 
     void AxeReturn(){
         to_hand_direction = HandTarget.position - transform.position; 
-        transform.position = Vector3.MoveTowards(transform.position, HandTarget.position, ReturnSpeed * Time.deltaTime); 
+        curve_eval = Mathf.Clamp01(curve_eval + Time.deltaTime * LateralReturnSpeed); 
+        float influence = LateralInfluence.Evaluate(curve_eval) * CurveMultiplier; 
+        Vector3 target_position = HandTarget.position * (1f - influence) + (HandTarget.position + lateral_hand_direction) * influence; 
+        transform.position = Vector3.MoveTowards(transform.position, target_position, ReturnSpeed * Time.deltaTime); 
+        RotateAxe(-ReturnRotationSpeed); 
+        // transform.position = Vector3.MoveTowards(transform.position, HandTarget.position, ReturnSpeed * Time.deltaTime); 
         CurrentHandDistance = Vector3.SqrMagnitude(to_hand_direction); 
         if(CurrentHandDistance < 1f){
             MainController.ReceiveAxe(); 
@@ -71,13 +90,19 @@ public class AxeBehaviour : MonoBehaviour{
 
     }
 
+    void RotateAxe(float speed){
+        Quaternion rot_value = Quaternion.AngleAxis(speed, RotationAxis); 
+        transform.rotation *= rot_value; 
+    }
+
     void AxeMovement(){
         transform.position = transform.position + ThrowDirection * Time.deltaTime; 
         ThrowDirection.y -= GravityForce*Time.deltaTime; 
 
         // Vector3 rot_axis = transform.rotation * RotationAxis.normalized;
-        Quaternion rot_value = Quaternion.AngleAxis(RotationSpeed, RotationAxis.normalized); 
-        transform.rotation = transform.rotation * rot_value; 
+        // Quaternion rot_value = Quaternion.AngleAxis(RotationSpeed, RotationAxis.normalized); 
+        // transform.rotation = transform.rotation * rot_value; 
+        RotateAxe(RotationSpeed); 
     }
 
 
